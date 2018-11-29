739932b0f6de1833dc1be6f77d940f1e45cf23a9
krilln <krilln@naver.com>
2018-11-29 14:52:45 +0900

    TypeHandler추가

---
+++ src/main/java/kr/or/yi/mybatis_dev_HGS/typehandler/PhoneTypeHandler.java
@@ -0,0 +1,39 @@
+package kr.or.yi.mybatis_dev_HGS.typehandler;
+
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.apache.ibatis.type.BaseTypeHandler;
+import org.apache.ibatis.type.JdbcType;
+
+import kr.or.yi.mybatis_dev_HGS.dto.PhoneNumber;
+
+public class PhoneTypeHandler extends BaseTypeHandler<PhoneNumber> {
+
+	@Override
+	public void setNonNullParameter(PreparedStatement ps, int i, PhoneNumber parameter, JdbcType jdbcType)
+			throws SQLException {
+	ps.setString(i, parameter.toString()); // "000-0000-0000"
+		
+	}
+
+	@Override
+	public PhoneNumber getNullableResult(ResultSet rs, String columnName) throws SQLException {
+		return new PhoneNumber(rs.getString(columnName));
+	}
+
+	@Override
+	public PhoneNumber getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
+		return new PhoneNumber(rs.getString(columnIndex));
+	}
+
+	@Override
+	public PhoneNumber getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
+		return new PhoneNumber(cs.getString(columnIndex));
+	}
+
+	
+
+}
